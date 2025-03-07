7bf272f44062cd42460e2cc28e6a682cece4b47c
Leandro Alves Maciel Filho <leandro.filho@sis-it.com>
2025-03-07 02:26:42 -0300

    att

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -39,17 +39,11 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  title: string = 'Quem somos?';
+  title: string = 'Quem somos';
   description: string = `Fundada por três psicólogos apaixonados pela profissão, a Contactus nasceu da crença no poder transformador da psicologia para promover qualidade de vida e bem-estar. Nosso trabalho é guiado pelas normas éticas da profissão e pelo conhecimento científico mais atualizado, aliado a recursos personalizados e de alta qualidade.  
 
 Na Contactus, o foco está no acolhimento das dificuldades de cada paciente, oferecendo um espaço seguro e empático para o desenvolvimento pessoal. Acreditamos no trabalho conjunto, onde paciente e terapeuta caminham lado a lado em busca de resultados concretos e relevantes, sempre apresentados de forma clara e transparente.  
 
 Seja para superar desafios emocionais, desenvolver autoconhecimento, alcançar uma vida mais equilibrada ou identificar um diagnóstico preciso, estamos aqui para ajudar você..`;
-  team = [
-    { name: 'Ana Silva', role: 'CEO' },
-    { name: 'Carlos Souza', role: 'CTO' },
-    { name: 'Mariana Costa', role: 'Designer' },
-    { name: 'Pedro Martins', role: 'Desenvolvedor' }
-  ];
 
 }
