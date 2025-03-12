749eb431079e3094837069401badff8bd065617e
Leandro Alves Maciel Filho <leandro.filho@sis-it.com>
2025-03-12 12:56:36 -0300

    att

--- src/app/components/welcome-modal/welcome-modal.component.ts
+++ src/app/components/welcome-modal/welcome-modal.component.ts
@@ -13,7 +13,7 @@ export class WelcomeModalComponent implements OnInit {
   ngOnInit(): void {
     setTimeout(() => {
       this.showModal = false;
-    }, 3000);
+    }, 4000);
   }
 
   fecharModal() {
