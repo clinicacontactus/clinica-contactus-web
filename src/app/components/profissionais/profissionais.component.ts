f50429f969b124441784888ac178720b1a9a62bb
Leandro Alves Maciel Filho <leandro.filho@sis-it.com>
2025-03-18 16:06:09 -0300

    att

--- src/app/components/profissionais/profissionais.component.ts
+++ src/app/components/profissionais/profissionais.component.ts
@@ -13,6 +13,8 @@ export class ProfissionaisComponent {
       titulo: 'Neuropsicólogo Clínico e Psicoterapeuta',
       subtitulo: 'CRP 11/18847',
       photoUrl: 'assets/imgs/especialistas/Jp.jpeg',
+      whatsapp: 'https://api.whatsapp.com/send?phone=5585991518663&text=Olá, João! Gostaria de mais informações!',
+      instagram: 'https://www.instagram.com/joaofreirepsi?igsh=MWI4YWR6MnkzcGV5Ng==',
       formacao: [
         'Graduado em Psicologia.',
         'Especialista em Neuropsicologia pela UNICHRISTUS.',
@@ -29,12 +31,14 @@ export class ProfissionaisComponent {
       titulo: 'Psicóloga Clínica e Supervisora ABA',
       subtitulo: 'CRP 11/18942',
       photoUrl: 'assets/imgs/especialistas/Salinas.jpeg',
+      whatsapp: 'https://api.whatsapp.com/send?phone=5585981656607&text=Olá, Salinas! Gostaria de mais informações!',
+      instagram: 'https://www.instagram.com/_psisalinasfreitas?igsh=MXF4YWh6N2dzbWJ0bg==',
       formacao: [
         'Psicóloga especializada em Terapia Cognitivo-Comportamental (TCC) e Análise do Comportamento Aplicada (ABA).',
         'Pós-graduanda em Neuropsicologia.',
         'Mestranda em Psicologia.'
       ],
-      atuacao: 'Sou psicóloga especializada em Terapia Cognitivo-Comportamental (TCC) e Análise do Comportamento Aplicada (ABA), com foco no atendimento a crianças, adolescentes e adultos. Atuo como supervisora ABA, dedicando-me especialmente ao trabalho com autismo, e possuo formação avançada em Terapia Cognitivo-Comportamental. Atualmente, estou ampliando meus conhecimentos como pós-graduanda em Neuropsicologia e mestranda em Psicologia, buscando integrar as mais recentes evidências científicas à minha prática clínica. Meu objetivo é oferecer um atendimento humanizado, ético e baseado em evidências, contribuindo para o desenvolvimento emocional, cognitivo e social dos meus pacientes.',
+      atuacao: 'Salinas Freitas é uma psicóloga especializada em Terapia Cognitivo-Comportamental (TCC) e Análise do Comportamento Aplicada (ABA), com foco no atendimento a crianças, adolescentes e adultos. Ela atua como supervisora ABA, dedicando-se especialmente ao trabalho com autismo, e possui formação avançada em Terapia Cognitivo-Comportamental. Atualmente, está ampliando seus conhecimentos como pós-graduanda em Neuropsicologia e mestranda em Psicologia, buscando integrar as mais recentes evidências científicas à sua prática clínica. Seu objetivo é oferecer um atendimento humanizado, ético e baseado em evidências, contribuindo para o desenvolvimento emocional, cognitivo e social dos seus pacientes. Seja para lidar com desafios do espectro autista, questões emocionais ou transtornos psicológicos, ela está comprometida em auxiliar no bem-estar psicológico de cada paciente.',
       publicoAlvo: [
         'Crianças, adolescentes e adultos.',
         'Pacientes com desafios do espectro autista e transtornos psicológicos.'
@@ -45,11 +49,13 @@ export class ProfissionaisComponent {
       titulo: 'Psicóloga Clínica e Supervisora ABA',
       subtitulo: 'CRP 11/19015',
       photoUrl: 'assets/imgs/especialistas/Mirella.jpeg',
+      whatsapp: 'https://api.whatsapp.com/send?phone=5585989828344&text=Olá, Mirella! Gostaria de mais informações!',
+      instagram: 'https://www.instagram.com/psimirellaoliveira?igsh=OGs0YWplZHF2aDdj',
       formacao: [
         'Psicóloga Clínica, especialista em Análise do Comportamento Aplicada (ABA).',
         'Especialização em Gestalt-Terapia e Educação Inclusiva.'
       ],
-      atuacao: 'Sou uma psicóloga apaixonada pelo processo de transformação e desenvolvimento humano. Possuo especialização em Análise do Comportamento Aplicada, Gestalt-Terapia e Educação Inclusiva, dedicando-me a apoiar e orientar aqueles que buscam superar desafios e encontrar um suporte emocional e mental. Atuo como psicóloga clínica, atendendo crianças, adolescentes e adultos, oferecendo um espaço acolhedor para o autoconhecimento e o acompanhamento de diversas questões. Além do trabalho clínico com psicoterapia e supervisão ABA, trabalho na área de Educação Inclusiva, refletindo meu profundo interesse e compromisso com a promoção de um ambiente mais justo e acolhedor para todos.',
+      atuacao: ' Mirella Sousa é uma psicóloga apaixonada pelo processo de transformação e desenvolvimento humano. Possui especialização em Análise do Comportamento Aplicada, Gestalt-Terapia e Educação Inclusiva, e tem se dedicado a apoiar e orientar aqueles que buscam superar desafios e encontrar suporte emocional e mental. Atua como psicóloga clínica, atendendo crianças, adolescentes e adultos, oferecendo um espaço acolhedor para o autoconhecimento e o acompanhamento de diversas questões. Acredita em sua capacidade de se conectar com o outro de forma verdadeira e respeitosa, o que realmente a destaca. Além do trabalho clínico com psicoterapia e supervisão ABA, trabalha na área de Educação Inclusiva, reflexo do seu profundo interesse e compromisso com a promoção de um ambiente mais justo e acolhedor para todos. ',
       publicoAlvo: [
         'Crianças, adolescentes e adultos.',
         'Pacientes em busca de autoconhecimento e suporte emocional.'
--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class SliderComponent {
 
-  @Input() dados: { name: string; titulo: string; subtitulo: string; photoUrl: string; formacao: string[]; atuacao: string; publicoAlvo: string[] }[] = [];
+  @Input() dados: { name: string; whatsapp: string; instagram: string; titulo: string; subtitulo: string; photoUrl: string; formacao: string[]; atuacao: string; publicoAlvo: string[] }[] = [];
 
   modalAberto: boolean = false;
 
@@ -21,15 +21,22 @@ export class SliderComponent {
       imagem: dado.photoUrl,
       formacao: dado.formacao,
       atuacao: dado.atuacao,
-      publicoAlvo: dado.publicoAlvo
+      publicoAlvo: dado.publicoAlvo,
+
+      whatsapp: dado.whatsapp,
+      instagram: dado.instagram,
     };
 
     this.modalAberto = true;
     document.body.style.overflow = 'hidden';
+    document.body.classList.add('no-scroll');
+
   }
 
   fecharModal() {
     this.modalAberto = false;
     document.body.style.overflow = '';
+    document.body.classList.remove('no-scroll');
   }
+
 }
