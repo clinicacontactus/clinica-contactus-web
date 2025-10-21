56bcadcd217802f248e6b0000bd945582efbd7df
leanford <leandroalvesmaciel@outlook.com>
2025-10-21 19:20:02 -0300

    att

--- src/App.jsx
+++ src/App.jsx
@@ -26,24 +26,25 @@ const App = () => {
   return (
     <div>
 
-      <div className="teste">
 
       <Navigation />
       <Header data={landingPageData.Header} />
 
-      {/* <Features data={landingPageData.Features} /> */}
+      <div className="teste-p">
+        {/* <Features data={landingPageData.Features} /> */}
 
-      <About data={landingPageData.About} />
-      {/* <Services data={landingPageData.Services} /> */}
-      <Gallery data={landingPageData.Gallery} />
-      {/* <Testimonials data={landingPageData.Testimonials} /> */}
-      <Team data={landingPageData.Team} />
-      <Contact data={landingPageData.Contact} />
+        <About data={landingPageData.About} />
+        {/* <Services data={landingPageData.Services} /> */}
+        <Gallery data={landingPageData.Gallery} />
+        {/* <Testimonials data={landingPageData.Testimonials} /> */}
+        <Team data={landingPageData.Team} />
+
       </div>
 
-
-
+      <Contact data={landingPageData.Contact} />
     </div>
+
+
   );
 };
 
--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -148,19 +148,19 @@ export const Contact = (props) => {
                 <ul>
                   <li>
                     <a href={props.data ? props.data.facebook : "/"}>
-                      <i className="fa fa-facebook"></i>
+                      <i className="fa fa-instagram"></i>
                     </a>
                   </li>
                   <li>
                     <a href={props.data ? props.data.twitter : "/"}>
-                      <i className="fa fa-twitter"></i>
+                      <i className="fa fa-whatsapp"></i>
                     </a>
                   </li>
-                  <li>
+                  {/* <li>
                     <a href={props.data ? props.data.youtube : "/"}>
                       <i className="fa fa-youtube"></i>
                     </a>
-                  </li>
+                  </li> */}
                 </ul>
               </div>
             </div>
