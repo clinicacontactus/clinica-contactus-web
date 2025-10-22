dda62200596115f5b7a4f515ba043b3f23e5d281
leanford <leandroalvesmaciel@outlook.com>
2025-10-22 16:48:07 -0300

    troca header

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-           <img src="img/logo-contactus-branca.png" className="img-header" alt="" />{" "}
+           <img src="img/imgs/new-logo.png" className="img-header" alt="" />{" "}
           </a>{" "}
         </div>
 
