af4c46aaa4b3c93df6dcf128b9ffb98181cdd69a
leanford <leandroalvesmaciel@outlook.com>
2025-10-26 23:59:27 -0300

    att

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,4 +1,16 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { faUserMd, faChild, faBrain, faStethoscope, faUsers, faBuilding } from '@fortawesome/free-solid-svg-icons';
+
+export const ICONS_MAP = {
+  "fa fa-user-md": faUserMd,
+  "fa fa-child": faChild,
+  "fa fa-brain": faBrain,
+  "fa fa-stethoscope": faStethoscope,
+  "fa fa-users": faUsers,
+  "fa fa-building": faBuilding,
+};
 
 export const Services = (props) => {
   return (
@@ -14,15 +26,26 @@ export const Services = (props) => {
         <div className="row">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-4">
-                  {" "}
-                  <i className={d.icon}></i>
-                  <div className="service-desc">
-                    <h3>{d.name}</h3>
-                    <p>{d.text}</p>
-                  </div>
+              <div key={`${d.name}-${i}`} className="col-md-4">
+                <FontAwesomeIcon
+                  icon={ICONS_MAP[d.icon]}
+                  size="4x"
+                  style={{
+                    backgroundColor: '#A0BCB5',
+                    color: '#fff',
+                    padding: '20px',
+                    width: '50px',
+                    height: '50px',
+                    borderRadius: '50%',
+                    marginBottom: '10px'
+                  }}
+                />
+                <div className="service-desc">
+                  <h3>{d.name}</h3>
+                  <p>{d.text}</p>
                 </div>
-              ))
+              </div>
+            ))
             : "loading"}
         </div>
       </div>
